b77591aa9a82ed68158303b770a2b76463f48d83
maazhussaini <skr2570@gmail.com>
2024-04-03 08:19:51 +0200

    update

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import io
 
 st.set_page_config(page_title="Munajat-e-Maqbool", page_icon="data/favicon.ico", layout="wide")
 
-def Munajaat(zoom, page_title):
+def Munajaat():
     
     # Path to PDF file
     pdf_file_path = "data/Munajat-e-MaqboolByShaykhAshrafAliThanvir.a.pdf"
@@ -20,7 +20,7 @@ def Munajaat(zoom, page_title):
                 pdf_bytes = pdf_file.read()
                 st.download_button(label="Download PDF",
                                 data=pdf_bytes,
-                                file_name=f"{page_title}.pdf",
+                                file_name=f"Munajat-e-Maqbool.pdf",
                                 mime="application/pdf",
                                 use_container_width=True)
                 
@@ -32,9 +32,6 @@ def Munajaat(zoom, page_title):
         left, center, right = st.columns([1, 7, 1])  
         with center:
             st.divider()
-
-    # st.divider()
-    # st.write("")
     
     if pdf_file_path:
         # Open the PDF file directly from the file path
@@ -46,11 +43,20 @@ def Munajaat(zoom, page_title):
             left_spacer, content, right_spacer = st.columns([1,3,1])  # Adjust the ratio as needed
 
             with content:  # This ensures content is in the middle column
+                
+                # Initialize the progress bar
+                progress_bar = st.progress(0)
+                
                 # Display each page of the PDF as an image
                 for page_num in range(pdf_document.page_count):
+                    
+                    # Update the progress bar
+                    progress_bar.progress((page_num + 1) / pdf_document.page_count)
+                    
                     page = pdf_document.load_page(page_num)  # Load the page
-                    # Define zoom factor: 5x zoom
-                    # zoom = 3
+                    
+                    # Define zoom factor: 1.75x zoom
+                    zoom = 1.75
                     mat = fitz.Matrix(zoom, zoom)  # Zoom factor for both x and y axes
                     
                     pix = page.get_pixmap(matrix=mat)  # Render page to an image with zoom
@@ -58,21 +64,33 @@ def Munajaat(zoom, page_title):
                     # st.image(image_stream, caption=f"Page {page_num + 1}", use_container_width=True)
                     st.image(image_stream, caption=f"Page {page_num + 1}", output_format = "auto")  # Adjust width as needed
 
+                progress_bar.empty()
 
-def Zariat(zoom, page_title):
-    st.title(page_title)
-    # Using a wide page layout
-
+def Zariat():
     # Path to PDF file
     pdf_file_path = "data/Zariatul-Wusool.pdf"
 
-    # Provide a download button for the entire PDF file
-    with open(pdf_file_path, "rb") as pdf_file:
-        pdf_bytes = pdf_file.read()
-        st.download_button(label="Download PDF",
-                            data=pdf_bytes,
-                            file_name=f"{page_title}.pdf",
-                            mime="application/pdf")
+    with st.container():
+        
+        col1, col2, col3, col4 = st.columns([1, 5, 2, 1])
+        
+        with col3:
+            # Provide a download button for the entire PDF file
+            with open(pdf_file_path, "rb") as pdf_file:
+                pdf_bytes = pdf_file.read()
+                st.download_button(label="Download PDF",
+                                    data=pdf_bytes,
+                                    file_name=f"Zariatul-Wusool.pdf",
+                                    mime="application/pdf")
+                
+            
+        
+        with col2:
+            st.header("Zariatul-Wusool")
+        
+        left, center, right = st.columns([1, 7, 1])  
+        with center:
+            st.divider()
     
     if pdf_file_path:
         # Open the PDF file directly from the file path
@@ -84,11 +102,20 @@ def Zariat(zoom, page_title):
             left_spacer, content, right_spacer = st.columns([1,3,1])  # Adjust the ratio as needed
 
             with content:  # This ensures content is in the middle column
+                
+                # Initialize the progress bar
+                progress_bar = st.progress(0)
+                
                 # Display each page of the PDF as an image
                 for page_num in range(pdf_document.page_count):
+                    
+                    # Update the progress bar
+                    progress_bar.progress((page_num + 1) / pdf_document.page_count)
+                    
                     page = pdf_document.load_page(page_num)  # Load the page
-                    # Define zoom factor: 5x zoom
-                    # zoom = 3
+                    
+                    # Define zoom factor: 1.75x zoom
+                    zoom = 1.75
                     mat = fitz.Matrix(zoom, zoom)  # Zoom factor for both x and y axes
                     
                     pix = page.get_pixmap(matrix=mat)  # Render page to an image with zoom
@@ -96,6 +123,7 @@ def Zariat(zoom, page_title):
                     # st.image(image_stream, caption=f"Page {page_num + 1}", use_container_width=True)
                     st.image(image_stream, caption=f"Page {page_num + 1}", output_format = "auto")  # Adjust width as needed
 
+                progress_bar.empty()
 
 
 def main():
@@ -105,12 +133,10 @@ def main():
         ("Munajat-e-Maqbool", "Zariatul-Wusool")
     )
 
-    zoom = st.sidebar.slider('Zoom', 1.75, 4.5)
-
     if add_selectbox == "Munajat-e-Maqbool":
-        Munajaat(zoom, add_selectbox)
+        Munajaat()
     else:
-        Zariat(zoom, add_selectbox)
+        Zariat()
     
 if __name__ == "__main__":
     main()
